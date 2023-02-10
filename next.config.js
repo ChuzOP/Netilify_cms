1252d4e68bc79c1567c52de57acd5a4c1d6130d1
chuso <dejesusgarciaop@gmail.com>
2023-02-09 21:54:05 -0600

    adding markdown library

--- next.config.js
+++ next.config.js
@@ -3,4 +3,15 @@ const nextConfig = {
   reactStrictMode: true,
 }
 
-module.exports = nextConfig
+module.exports = {
+  webpack: (cfg) => {
+      cfg.module.rules.push(
+          {
+              test: /\.md$/,
+              loader: 'frontmatter-markdown-loader',
+              options: { mode: ['react-component'] }
+          }
+      )
+      return cfg;
+  }
+}
